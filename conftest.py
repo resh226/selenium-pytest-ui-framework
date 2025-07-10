2c9e61dd4dd1c0276871b71fda84f9b909ab037f
resh226 <reshmasajeevpulickal@gmail.com>
2025-07-10 21:04:05 +0400

    execute

--- conftest.py
+++ conftest.py
@@ -119,12 +119,12 @@ def browser(config):
         if browser_type == 'Chrome':
             options = ChromeOptions()
             # Add headless and CI-safe flags
-           # options.add_argument("--headless")
-           # options.add_argument("--disable-gpu")
-            #options.add_argument("--no-sandbox")
-           # options.add_argument("--disable-dev-shm-usage")
-            #options.add_argument("--window-size=1920,1080")
-           # options.add_argument("--user-data-dir=/tmp/chrome-profile")
+            options.add_argument("--headless")
+            options.add_argument("--disable-gpu")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--window-size=1920,1080")
+            options.add_argument("--user-data-dir=/tmp/chrome-profile")
             service = ChromeService(ChromeDriverManager().install())
             b = selenium.webdriver.Chrome(service=service, options=options)
         elif browser_type == 'Firefox':
